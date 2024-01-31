a01dd5c65d53729857f46759caf26d3dd32fbc23
Sahil <118194056+Sahil9446@users.noreply.github.com>
2024-02-01 00:05:00 +0530

    Create Convert from Celsius to Fahrenhiet.py

---
+++ Convert from Celsius to Fahrenhiet.py
@@ -0,0 +1,13 @@
+#QUESTION DESCRIPTION
+"""
+Write a Python program to convert temperature from Celsius to Fahrenheit
+"""
+
+#SOLUTION
+a=float(input("Enter the temperature in celcius: "))
+b=(a*9/5)+32
+print(a,"Celcius is equal to",b,"Fahrenheit")
+
+#OUTPUT
+# Enter the temperature in celcius: 24
+# 24.0 Celcius is equal to 75.2 Fahrenheit
