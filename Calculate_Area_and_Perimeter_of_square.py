d0c6f9446f025f66aaeb67a0b25be399677a56aa
Sahil <118194056+Sahil9446@users.noreply.github.com>
2024-02-02 19:07:43 +0530

    Create Calculate_Area_and_Perimeter_of_square.py

---
+++ Calculate_Area_and_Perimeter_of_square.py
@@ -0,0 +1,16 @@
+#QUESTION DESCRIPTION
+"""
+Write a Python program to calculate Area and Perimeter of Square.
+""""
+
+#SOLUTION
+a=float(input("Enter the Length of the square: "))
+b=a**2
+c=4*a
+print("The Area of the given square is:", b)
+print("The perimeter of the given square is:", c)
+
+#OUTPUT
+# Enter the Length of the square: 8
+# The Area of the given square is: 64.0
+# The perimeter of the given square is: 32.0
