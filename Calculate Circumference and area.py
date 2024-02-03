88c38c3ec1e32d8a97055ecbb35f2057a8f717fc
Sahil <118194056+Sahil9446@users.noreply.github.com>
2024-02-03 23:15:04 +0530

    Create Calculate Circumference and area.py

---
+++ Calculate Circumference and area.py
@@ -0,0 +1,21 @@
+#QUESTION DESCRIPTION
+"""
+Write a python program to enter radius of a circle and find its diameter, circumference and area.
+"""
+
+#SOLUTION
+a=float(input("Enter the Radius of the circle: "))
+pi= 3.141592653589793
+b=2*a
+c=2*pi*a
+d=pi*a**2
+print("The diameter of the given circle is:",b)
+print("The circumference of the given circle is:",c)
+print("The area of the given circle is:",d)
+
+#OUTPUT
+# Enter the Radius of the circle: 4.5
+# The diameter of the given circle is: 9.0
+# The circumference of the given circle is: 28.274333882308138
+# The area of the given circle is: 63.61725123519331
+
