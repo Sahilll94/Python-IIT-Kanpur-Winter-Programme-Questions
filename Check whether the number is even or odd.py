ae432d9feea05c5f5833b88b2b7e801adc0d7407
Sahil <118194056+Sahil9446@users.noreply.github.com>
2024-02-11 22:37:29 +0530

    Create Check whether the number is even or odd.py

---
+++ Check whether the number is even or odd.py
@@ -0,0 +1,15 @@
+#QUESTION DESCRIPTION
+"""
+Write a Python program to check whether a number is even or odd.
+"""
+
+#SOLUTION
+x=int(input("Enter the number you wish to check whether it is even or not: "))
+if (x%2==0):
+    print("The number is even")
+else:
+    print("The number is odd")
+
+#OUTPUT
+# Enter the number you wish to check whether it is even or not: 3
+# The number is odd
