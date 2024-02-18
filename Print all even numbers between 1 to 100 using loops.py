a63a9cecf7c85671752d8880d008719907eba521
Sahil <118194056+Sahil9446@users.noreply.github.com>
2024-02-18 23:13:39 +0530

    Create Print all even numbers between 1 to 100 using loops.py

---
+++ Print all even numbers between 1 to 100 using loops.py
@@ -0,0 +1,13 @@
+#Question Description
+"""
+Write a Python program to print all even numbers between 1 to 100 using loop.
+"""
+
+#Solution
+print("even numbers from 1 to 100 are:")
+for num in range(2,101,2):
+    print(num,end=",") #Remember. the last even mumber will also have comma(,)
+
+#Output
+# even numbers from 1 to 100 are:
+# 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74,76,78,80,82,84,86,88,90,92,94,96,98,100,
