f37ea81ed2477810225a5c55892a25fc18ba9258
Sahil <118194056+Sahil9446@users.noreply.github.com>
2024-02-21 00:34:57 +0530

    Update Addition_of_two_number.py

--- Addition_of_two_number.py
+++ Addition_of_two_number.py
@@ -1,15 +1,15 @@
-#QUESTION DESCRIPTION
+#AIM
 """
 Write a Python program to Addition of Two Number.
 """
 
-#SOLUTION
+#PROGRAM
 a=int(input("Enter the first number: "))
 b=int(input("Enter the second number: "))
 c=a+b
 print("The sum of two number is", c)
 
-#OUTPUT
+# INPUT & OUTPUT
 # Enter the first number: 4
 # Enter the second number: 6
 # The sum of two number is 10
