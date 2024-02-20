dd664cd8e01dd032fff6dc2972d379128fe2acb3
Sahil <118194056+Sahil9446@users.noreply.github.com>
2024-02-20 21:27:28 +0530

    Create Star pattern pyramid.py

---
+++ Star pattern pyramid.py
@@ -0,0 +1,19 @@
+#Question Description
+"""
+Create a normal Star Pattern Pyramid in python.
+"""
+
+#Solution
+print("Normal star pattern")
+for i in range(5):
+  x="*"
+  x=x*i
+  print(x)
+
+# Output
+# Normal Pyramid
+
+# *
+# **
+# ***
+# ****
