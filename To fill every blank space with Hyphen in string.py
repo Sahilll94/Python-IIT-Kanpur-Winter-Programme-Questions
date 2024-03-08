1f915aaa0731f167b56bf0fd943a1628cc9a05df
Sahil <118194056+Sahil9446@users.noreply.github.com>
2024-03-09 00:34:57 +0530

    Create To fill every blank space with Hyphen in string.py

---
+++ To fill every blank space with Hyphen in string.py
@@ -0,0 +1,14 @@
+#Question Description
+"""
+Python Program to Take in a String and Replace Every Blank Space with Hyphen.
+"""
+
+#Solution
+str=input("Enter the String: ")
+
+modified_str=str.replace("","-")
+print("Modified string is",modified_str)
+
+#Output
+# Enter the String: SAHIL
+# Modified string is -S-A-H-I-L-
