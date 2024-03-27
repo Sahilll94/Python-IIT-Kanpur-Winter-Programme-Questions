d089c6b030194ed938644fd0135d664c3fe7c74a
Sahil <118194056+Sahilll94@users.noreply.github.com>
2024-03-27 21:12:19 +0530

    Create Calculate the length of a string.py

---
+++ Calculate the length of a string.py
@@ -0,0 +1,23 @@
+#Question description
+"""
+Python Program to Calculate the Length of a String Without Using a Library Function.
+"""
+
+#Solution
+User_string=input("Enter the string: ")
+
+length=0
+
+for char in User_string:
+    length+=1
+
+
+print(f"The length of the given string {User_string} is {length}")
+
+
+#Output
+= RESTART: E:/WTP IITK 23/Assignment 4 (String)/length of the string without using library func.py
+# Enter the string: sahil
+# The length of the given string sahil is 5
+
+
