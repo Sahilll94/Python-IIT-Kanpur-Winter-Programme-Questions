f61366a710438bf2b470296dd610d600b6fa0800
Sahil <118194056+Sahilll94@users.noreply.github.com>
2024-05-05 02:09:54 +0530

    Create Printing the minimum key using its value.py

---
+++ Printing the minimum key using its value.py
@@ -0,0 +1,16 @@
+#Question overview
+sample_dict = {'Physics': 82, 'Math': 65, 'history': 75}
+
+min_key = min(sample_dict, key=lambda k: sample_dict[k])
+print(min_key)
+
+# Code
+sample_dict = {'Physics': 82, 'Math': 65, 'history': 75}
+
+min_key = min(sample_dict, key=lambda k: sample_dict[k])
+print(min_key)
+
+#Output
+Math
+
+
